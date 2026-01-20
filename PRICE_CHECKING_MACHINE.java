4b70c7b295c7b0677ee1a0404cee3ca885692340
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-01-20 23:13:15 +0500

    fixo

---
+++ PRICE_CHECKING_MACHINE.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class PRICE_CHECKING_MACHINE {
+    public static void main(String[] args) {
+
+        HashMap<String, Integer> PRODUCT_DICTIONARY = new HashMap<>();
+        PRODUCT_DICTIONARY.put("PN1", 1);
+        PRODUCT_DICTIONARY.put("PN2", 2);
+        PRODUCT_DICTIONARY.put("PN3", 3);
+        PRODUCT_DICTIONARY.put("PN4", 4);
+        PRODUCT_DICTIONARY.put("PN5", 5);
+        PRODUCT_DICTIONARY.put("PN6", 6);
+        System.out.println(PRODUCT_DICTIONARY);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter product name (or 'quit' to stop):");
+        String p_n = scanner.nextLine();
+
+        if(p_n.equals("quit") || p_n.equals("stop")){
+            System.out.println("Program terminated by user.");
+        }
+        else if(PRODUCT_DICTIONARY.containsKey(p_n)) {
+            System.out.println("Product:" + p_n + "\nProduct Price:" + PRODUCT_DICTIONARY.get(p_n));
+        }
+        else {
+            System.out.println("Product is not available");
+        }
+    }
+}
+
